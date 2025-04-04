e34c525906fbab1948e8038939529fd5c9bd1ea6
Juan Camilo Losada Bultaif <92903961+jclosadab1996@users.noreply.github.com>
2025-04-04 00:53:00 -0500

    add some description and new components for home page

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,19 @@
+import { Link } from "lucide-react";
 import React from "react";
+import Image from "next/image";
 
 const Rootlayout = ({ children }: { children: React.ReactNode }) => {
-  return <div>{children}</div>;
+  return (
+    <div className="root-layout)">
+      <nav>
+        <Link href="/" className="flex items-center gap-2" />
+        <Image src="/logo.svg" alt="logo" height={32} width={38} />
+        <h2 className="text-primary-100">PrepWise</h2>
+      </nav>
+
+      {children}
+    </div>
+  );
 };
 
 export default Rootlayout;
